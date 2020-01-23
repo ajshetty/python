c5676832a95b564a20c46675c9b19397b8990a65
Akshit Shetty <akshit.j.shetty@gmail.com>
2020-01-23 13:59:41 -0500

    Add files via upload

---
+++ ps0ajsSubmisison.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 23:33:51 2020
+
+@author: AShetty
+"""
+import numpy
+
+print ("hello there ")
+
+x = int (input ("enter the number x: "))
+
+y = int (input ("enter the number y:  "))
+
+print(x**y)
+
+print (numpy.log2(x))
